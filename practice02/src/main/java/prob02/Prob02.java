42bca9d8c9f7b85e9b98bd514ad1e1ee930c334a
kimhaengun <chxl89@naver.com>
2021-09-08 18:48:43 +0900

    practice02-2

--- practice02/src/main/java/prob02/Prob02.java
+++ practice02/src/main/java/prob02/Prob02.java
@@ -11,11 +11,18 @@ public class Prob02 {
 		double sum = 0;
 
 		/* 키보드에서 배열 크기만큼 입력 받아 배열에 저장하는 코드 */
-		
+		System.out.println("5개의 숫자를 입력하세요.");
+		for (int i = 0; i < intArray.length; i++) {
+			intArray[i] = scanner.nextInt();
+		}
 		/* 배열에 저장된 정수 값 더하기 */
-		
+		for (int i = 0; i < intArray.length; i++) {
+			sum+=intArray[i];
+			
+		}
 		/* 출력 */
-		
+		sum = sum/intArray.length;
+		System.out.println(sum);
 		/* 자원정리 */
 		scanner.close();
 	}
