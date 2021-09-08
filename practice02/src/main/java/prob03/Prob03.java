9f31ab53f1f4140fae5ec43db284792cab49f5cd
kimhaengun <chxl89@naver.com>
2021-09-08 19:25:18 +0900

    practice02-3

--- practice02/src/main/java/prob03/Prob03.java
+++ practice02/src/main/java/prob03/Prob03.java
@@ -3,7 +3,6 @@ package prob03;
 public class Prob03 {
 	public static void main(String args[]) {
 		char c[] = { 'T', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ', 'p', 'e', 'n', 'c', 'i', 'l', '.' };
-
 		// 원래 배열 원소 출력
 		printCharArray(c);
 
@@ -12,6 +11,26 @@ public class Prob03 {
 
 		// 수정된 배열 원소 출력
 		printCharArray(c);
+	}
+
+	private static void replaceSpace(char[] c) {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < c.length; i++) {
+			if (c[i]==' ') {
+				c[i]=',';
+			}
+			if(c[i]=='.') {
+				c[i]=' ';
+			}
+		}
+	}
+
+	private static void printCharArray(char[] c) {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < c.length; i++) {
+			System.out.print(c[i]);
+		}
+		System.out.println("");
 	}
 
 }
