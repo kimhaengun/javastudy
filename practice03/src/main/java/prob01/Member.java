c9d897e37ce51b4796ad7adba5945b9977c087f1
kimhaengun <chxl89@naver.com>
2021-09-09 18:40:20 +0900

    practice03-1

--- practice03/src/main/java/prob01/Member.java
+++ practice03/src/main/java/prob01/Member.java
@@ -1,4 +1,38 @@
 package prob01;
 
 public class Member {
+	//회원 아이디
+	private String id;
+	//회원 이름
+	private String name;
+	//회원 point
+	private int point;
+
+	//생성자
+	public Member() {
+		
+	}
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPoint() {
+		return point;
+	}
+
+	public void setPoint(int point) {
+		this.point = point;
+	}
 }
