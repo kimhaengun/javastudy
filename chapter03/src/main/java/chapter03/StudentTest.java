69f8f3736a4ca4c8c69e7ae1362200a751ec9858
kimhaengun <chxl89@naver.com>
2021-09-09 14:08:06 +0900

    다운, 업 캐스팅

--- chapter03/src/main/java/chapter03/StudentTest.java
+++ chapter03/src/main/java/chapter03/StudentTest.java
@@ -3,7 +3,21 @@ package chapter03;
 public class StudentTest {
 
 	public static void main(String[] args) {
-		new Student(); //실행 순서 1.부모 2.자식 생성자
+		Student s1 = new Student(); //실행 순서 1.부모 2.자식 생성자
+		s1.setGrade(1);
+		s1.setMajor("학생");
+		//<upcasting>
+		//name값은 부모 값 -> 실질적 ((person)s1).setName("둘리"); 
+//		s1.setName("둘리"); 
+		
+		//upcasting(암시적, Implicity)
+		Person p1 =s1;
+		p1.setName("둘리");
+		
+		//<downcasting>
+		//downcasting(명시적,explicity)
+		Student s2 = (Student)p1;
+		s2.setMajor("학생");
 		
 	}
 
