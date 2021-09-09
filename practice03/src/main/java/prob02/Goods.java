d23b220f3059e02199a48c02ab63634f9ad3b88a
kimhaengun <chxl89@naver.com>
2021-09-09 19:24:42 +0900

    practice03-2

---
+++ practice03/src/main/java/prob02/Goods.java
@@ -0,0 +1,26 @@
+package prob02;
+
+public class Goods {
+	private String product; //상품명
+	private int price; //상품 가격
+	private int amount; //상품 양
+	
+	public String getProduct() {
+		return product;
+	}
+	public void setProduct(String product) {
+		this.product = product;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+}
--- practice03/src/main/java/prob02/GoodsApp.java
+++ practice03/src/main/java/prob02/GoodsApp.java
@@ -1,5 +1,6 @@
 package prob02;
 
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class GoodsApp {
@@ -11,9 +12,21 @@ public class GoodsApp {
 		Goods[] goods = new Goods[COUNT_GOODS];
 
 		// 상품 입력
-
+		for (int i = 0; i < goods.length; i++) {
+			String[] str = scanner.nextLine().split(" ");
+			String product = str[0];
+			int price = Integer.parseInt(str[1]);
+			int amount = Integer.parseInt(str[2]);
+			goods[i] = new Goods();
+			goods[i].setProduct(product);
+			goods[i].setPrice(price);
+			goods[i].setAmount(amount);
+		}
 		// 상품 출
-		
+		for (int i = 0; i < goods.length; i++) {
+			System.out.println(goods[i].getProduct()+"(가격:"+goods[i].getPrice()+"원)이 "+goods[i].getAmount()+"개 입고 되었습니다.");
+
+		}
 		// 자원정리
 		scanner.close();
 	}
