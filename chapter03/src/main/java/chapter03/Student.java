fa2136fbc0edc306bbf0f255c64092b3a8ccfb18
kimhaengun <chxl89@naver.com>
2021-09-09 14:35:56 +0900

    super 설명

--- chapter03/src/main/java/chapter03/Student.java
+++ chapter03/src/main/java/chapter03/Student.java
@@ -5,6 +5,9 @@ public class Student extends Person {
 	private String major;//전공
 	
 	public Student() {
+		//자식의 모든 생성자에서 부모의 특정 생성자를 명시하지않으면 
+		//암시적으로 부모의 기본 생성자가 자식 생성자 코드 앞에! 호출된다.
+		//super();
 		System.out.println("Student() called");
 	}
 	
