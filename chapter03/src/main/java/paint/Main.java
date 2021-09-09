e5d90eef767faa16e051e0b8402f496d7663f00d
kimhaengun <chxl89@naver.com>
2021-09-09 15:05:41 +0900

    <instanceof 연산자>

--- chapter03/src/main/java/paint/Main.java
+++ chapter03/src/main/java/paint/Main.java
@@ -41,7 +41,24 @@ public class Main {
 		//Graphic
 		GraphicText graphicText = new GraphicText("Hello World");
 		draw(graphicText);
+		
+		//=<instanceof>=
+		//instanceof test -> 부모, 조상까지 가능 but 형제는 불가능
+		//Object(Shape(Triangle,Rectangle,Circle))
+		System.out.println(circle instanceof Object);
+		System.out.println(circle instanceof Shape);
+		System.out.println(circle instanceof Circle);
+		//오류 -> 형제 불가능
+		//class는 hierachy 상위와 하위만 instanceof 연산자를 사용할 수 있다.
+//		System.out.println(circle instanceof Rectangle);
+		
+		//인터페이스(interface)는 hierachy와 상관없이 instanceof 연산자를 사용할 수 있다
+		System.out.println(circle instanceof Drawable);
+		//run함수 없음 - false 
+		System.out.println(circle instanceof Runnable);
+		
 	}
+	
 	public static void draw(Drawable drawble) {
 		drawble.draw();
 	}
@@ -69,6 +86,7 @@ public class Main {
 //	public static void drawCircle(Circle circle) {
 //		circle.draw();
 //	}
+	
 	
 
 }
